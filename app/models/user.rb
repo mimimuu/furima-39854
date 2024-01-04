0aa54df2bc2524f8c3d0caf02ddc7d2ebacc2621
mimimuu <mmm_nimo@yahoo.co.jp>
2024-01-04 21:53:39 +0900

    新規登録のエラー表示修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :password,
+            format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i,
+                      message: 'is invalid. Input half-width alphanumeric characters.' }
   validates :nickname, presence: true
   validates :birthday, presence: true
 
@@ -12,8 +15,5 @@ class User < ApplicationRecord
     validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters.' }
     validates :first_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width characters.' }
     validates :last_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width characters.' }
-    validates :password,
-              format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i,
-                        message: 'is invalid. Input half-width alphanumeric characters.' }
   end
 end
