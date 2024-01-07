14e03f95e1b61b2ed610c763ac9e25aa5e783765
mimimuu <mmm_nimo@yahoo.co.jp>
2024-01-07 16:16:44 +0900

    商品情報不足時のエラーメッセージ表示修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,6 +17,6 @@ class Item < ApplicationRecord
   validates :shipping_fee_burden_id,   presence: true, numericality: { other_than: 1, message: "can't be blank"}
   validates :prefecture_id,            presence: true, numericality: { other_than: 0, message: "can't be blank"}
   validates :days_until_shipping_id,   presence: true, numericality: { other_than: 1, message: "can't be blank"}
-  validates :selling_price,            presence: true, numericality: { in: 300..9999999, message: "must be greater than or equal to 300"}, format: { with: /\A[0-9]+\z/, message: "not a number"}
+  validates :selling_price,            presence: true, numericality: { only_integer: true, in: 300..9999999}
   validates :image,                    presence: true
 end
