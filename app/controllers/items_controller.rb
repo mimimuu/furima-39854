ccdac0f0d09b6d8c476c4cc5294f9c6f8253d8ae
mimimuu <mmm_nimo@yahoo.co.jp>
2024-01-07 12:26:54 +0900

    商品出品機能MVC実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,24 @@
 class ItemsController < ApplicationController
   def index
   end
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(items_params)
+    if @item.valid?
+      @item.save
+      redirect_to root_path
+    else
+    end
+
+  end
+
+  private
+
+  def items_params
+    params.require(:item).permit(:name,:content,:category_id,:condition_id,:shipping_fee_burden_id,:prefecture_id,:days_until_shipping_id,:selling_price).merge(user_id: current_user.id)
+  end
+
 end
