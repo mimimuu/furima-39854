9bbe892b8228df244d4433469e1b8a310a49e809
mimimuu <mmm_nimo@yahoo.co.jp>
2024-01-10 22:17:23 +0900

    配送先保存実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,13 +11,14 @@ class OrdersController < ApplicationController
       @orderdelivery.save
       redirect_to root_path
     else
-      render :index, status: unprocessable_entity
+      @item = Item.find(params[:item_id])
+      render :index, status: :unprocessable_entity
     end
   end
 
   private
   def order_delivery
-    params.require(:order).permit(:post_code,:prefecture, :municipalities, :street_address, :building_name, :telephone_num).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_delivery).permit(:post_code,:prefecture_id, :municipalities, :street_address, :building_name, :telephone_num).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
 end
--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,6 +1,5 @@
 class Delivery < ApplicationRecord
+  belongs_to :order
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
-  belongs_to :order
-
 end
