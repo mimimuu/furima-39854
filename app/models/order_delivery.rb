fdec1db9e6322b7e4bdd069625b4c0c4fc447589
mimimuu <mmm_nimo@yahoo.co.jp>
2024-01-10 22:31:24 +0900

    電話番号バリデーション修正

--- app/models/order_delivery.rb
+++ app/models/order_delivery.rb
@@ -6,7 +6,7 @@ class OrderDelivery
     validates :post_code , format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
     validates :municipalities
     validates :street_address
-    validates :telephone_num,  numericality: {only_integer: true, message: "is invalid. Input only number"}, length: { is: 11 ,message: "is too short"}
+    validates :telephone_num,  numericality: {only_integer: true, message: "is invalid. Input only number", in: 10..11}
     validates :user_id
     validates :item_id
   end
