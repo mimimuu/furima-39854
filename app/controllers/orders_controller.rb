d9f66b272d2b538fc65dfc631fa67e1169a85837
mimimuu <mmm_nimo@yahoo.co.jp>
2024-01-11 22:38:40 +0900

    決済機能実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
 
   def index
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @item = Item.find(params[:item_id])
     if user_signed_in? && current_user.id != @item.user_id
       @orderdelivery = OrderDelivery.new
@@ -12,9 +13,11 @@ class OrdersController < ApplicationController
   def create
     @orderdelivery = OrderDelivery.new(order_delivery)
     if @orderdelivery.valid?
+      pay_item
       @orderdelivery.save
-      redirect_to root_path
+      return redirect_to root_path
     else
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
       @item = Item.find(params[:item_id])
       render :index, status: :unprocessable_entity
     end
@@ -22,7 +25,16 @@ class OrdersController < ApplicationController
 
   private
   def order_delivery
-    params.require(:order_delivery).permit(:post_code,:prefecture_id, :municipalities, :street_address, :building_name, :telephone_num).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_delivery).permit(:post_code,:prefecture_id, :municipalities, :street_address, :building_name, :telephone_num).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    item = Item.find(params[:item_id])
+    Payjp::Charge.create(
+      amount: item.selling_price,
+      card: order_delivery[:token],
+      currency: 'jpy'
+    )
+  end
 end
--- app/models/order_delivery.rb
+++ app/models/order_delivery.rb
@@ -1,14 +1,15 @@
 class OrderDelivery
   include ActiveModel::Model
-  attr_accessor :post_code,:prefecture_id,:municipalities,:street_address,:building_name,:telephone_num,:user_id,:item_id
+  attr_accessor :post_code,:prefecture_id,:municipalities,:street_address,:building_name,:telephone_num,:user_id,:item_id,:token
 
   with_options presence: true do
     validates :post_code , format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
     validates :municipalities
     validates :street_address
-    validates :telephone_num,  numericality: {only_integer: true, message: "is invalid. Input only number", in: 10..11, message:"is too short"}
+    validates :telephone_num,  numericality: {only_integer: true, message: "is invalid. Input only number"}, length: {in: 10..11, message:"is too short"}
     validates :user_id
     validates :item_id
+    validates :token
   end
   validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank"}
 
